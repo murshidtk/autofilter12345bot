16fc08d06b84ba05f4758d4af9bae6689e37daf2
murshidtk <85290061+murshidtk@users.noreply.github.com>
2021-06-05 19:56:55 +0300

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption =  f" {file_name} \n @MALAYALAMCINEMA2255",,
+                caption =  f" {file_name} \n @MALAYALAMCINEMA2255",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
