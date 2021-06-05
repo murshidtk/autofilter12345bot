c49b6b32d448ea710b70a7fea5a2ba7c20fc8dae
murshidtk <85290061+murshidtk@users.noreply.github.com>
2021-06-05 19:51:40 +0300

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = caption,
+                caption =  f" {file_name} \n @MALAYALAMCINEMA2255",,
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    ' 🌟 Our group 🌟 ', url="https://t.me/MALAYALAMCINEMA2255"
                                 )
                         ]
                     ]
